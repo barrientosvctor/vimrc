1d0a70438216438da8516dc9ea2599bbc0a49ba6
barrientosvctor <vibarrientosp@gmail.com>
2024-06-02 20:45:05 -0500

    Support for parameters on pwsh script
    The last way to put a type of action via stdin will not longer used

--- scripts/actions.ps1
+++ scripts/actions.ps1
@@ -1,20 +1,24 @@
-Write-Host "What you want to do?"
-Write-Host "1) Install modules"
-Write-Host "2) Update modules"
-Write-Host "3) Uninstall modules"
+Param(
+	[Parameter(Mandatory, HelpMessage={
+		'Type a number:'
+		'1) Install modules'
+		'2) Update modules'
+		'3) Uninstall modules'
+		}
+	)]
+	[int]$ActionNumber
+)
 
-[int]$userInput = Read-Host "Type a number"
-
-If (($userInput -lt 1) -or ($userInput -gt 3)) {
-    Write-Host "Invalid option."
-} ElseIf ($userInput -eq 1) {
+If (($ActionNumber -lt 1) -or ($ActionNumber -gt 3)) {
+    Write-Host "Invalid option." -ForegroundColor Red
+} ElseIf ($ActionNumber -eq 1) {
     git submodule init
     git submodule update
-    Write-Host "Modules has been successfully installed!"
-} ElseIf ($userInput -eq 2) {
+    Write-Host "Modules has been successfully installed!" -ForegroundColor Green
+} ElseIf ($ActionNumber -eq 2) {
     git submodule update --remote
-    Write-Host "Modules has been successfully updated to the latest release!"
-} ElseIf ($userInput -eq 3) {
+    Write-Host "Modules has been successfully updated to the latest release!" -ForegroundColor Green
+} ElseIf ($ActionNumber -eq 3) {
     git submodule deinit --all
-    Write-Host "Modules has been uninstalled."
+    Write-Host "Modules has been uninstalled." -ForegroundColor Green
 }
